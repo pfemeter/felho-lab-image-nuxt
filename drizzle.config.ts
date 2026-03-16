07ce76ca8a3296902e728e6ad07fe062aa737661
Marton Pfemeter <pfemeter.marton@icloud.com>
2026-03-16 08:55:07 +0100

    updating drizzle config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -6,5 +6,8 @@ export default defineConfig({
     dialect: 'postgresql',
     dbCredentials: {
         url: process.env.DATABASE_URL!,
+        ssl: {
+            rejectUnauthorized: false,
+        },
     },
 });
